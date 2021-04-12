7d21cd3b788c26c5e56b2b09cb088580824c09a1
Reto Schmucki <reto.schmucki@mail.mcgill.ca>
2021-04-12 17:53:49 +0100

    remove nc data from data forlder

--- R/data.R
+++ R/data.R
@@ -6,11 +6,3 @@ utils::globalVariables(c("ecad_version"))
 #'   \item{ecad_version}{version number used to gather ECAD data}
 #' }
 "ecad_version"
-
-#' Subset of mean temperature ECAD v.23.1e for Switzerland 2011-2020
-#' @format NetCD format
-#' \describe{
-#'   \item{tg_ens_mean_0.25_subset_CH.nc}{tg subset for Switzerland}
-#' }
-
-"tg_ens_mean_0.25_subset_CH.nc"
